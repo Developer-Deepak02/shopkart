443af791861ac58967091988ac1299e98deb9017
Developer-Deepak02 <db23806@gmail.com>
2025-09-05 12:16:26 +0530

    feat: add admin sidebar menu and routes
    - Created a new file `adminSidebarMenu.js` to define the sidebar menu for the admin panel, including links for Dashboard, Categories, Products, Coupons, Orders, Customers, Ratings & Reviews, and Media.
    - Added new routes in `AdminPanelRoute.js` for the admin dashboard.
    - Updated `WebsiteRoute.js` to include a new user dashboard route and ensured the reset password route is properly formatted.
    - Updated `package.json` and `package-lock.json` to include new dependencies for Radix UI components.

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Card, CardContent } from "@/components/ui/card";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import Logo from "@/public/assets/logo-black.png";
 import Image from "next/image";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -20,15 +20,20 @@ import ButtonLoading from "@/components/Application/ButtonLoading";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa6";
 import Link from "next/link";
-import { WEBSITE_REGISTER, WEBSITE_RESETPASSWORD } from "@/routers/WebsiteRoute";
+import { USER_DASHBOARD, WEBSITE_REGISTER, WEBSITE_RESETPASSWORD } from "@/routers/WebsiteRoute";
 import axios from "axios";
 import { showToast } from "@/lib/showToast";
 import OTPVerification from "@/components/Application/OTPVerification";
 import { useDispatch } from "react-redux";
 import { login } from "@/store/reducer/authReducer";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { ADMIN_DASHBOARD } from "@/routers/AdminPanelRoute";
 
 const Loginpage = () => {
 	const dispatch = useDispatch();
+	const searchParams = useSearchParams();
+	const router = useRouter();
 	const [loading, setLoading] = useState(false);
 	const [otpVerificationLoading, setOtpVerificationLoading] = useState(false);
 	const [isTypePassword, setIsTypePassword] = useState(true);
@@ -87,6 +92,15 @@ const Loginpage = () => {
 			showToast("success", otpResponse.message);
 			// update redux store
 			dispatch(login(otpResponse.data));
+			if(searchParams.has("callback")){
+				router.push(searchParams.get("callback"));
+			}else{
+			otpResponse.data.role === "admin"
+				? router.push(ADMIN_DASHBOARD)
+				: router.push(USER_DASHBOARD);	
+			
+			}
+
 		} catch (error) {
 			showToast("error", error.message);
 		} finally {
---
+++ components/Application/Admin/AppSidebar.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+} from "@/components/ui/sidebar";
+import logoBlack from "@/public/assets/logo-black.png";
+import logoWhite from "@/public/assets/logo-white.png";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { LuChevronRight } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
+import { adminAppSidebarMenu } from "@/lib/adminSidebarMenu";
+import {
+	Collapsible,
+	CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import Link from "next/link";
+import { CollapsibleContent } from '@radix-ui/react-collapsible';
+
+
+const AppSidebar = () => {
+	return (
+		<Sidebar>
+			<SidebarHeader className="border-b h-17 mb-2 p-0">
+				<div className="flex items-center justify-between gap-6 px-2">
+					<Image
+						src={logoBlack}
+						height={70}
+						className="block dark:hidden"
+						alt="logo"
+					/>
+					<Image
+						src={logoWhite}
+						height={70}
+						className="hidden dark:block"
+						alt="logo"
+					/>
+					<Button type="button" size="icon" className="">
+						<IoMdClose />
+					</Button>
+				</div>
+			</SidebarHeader>
+
+			<SidebarContent className="p-3">
+				<SidebarMenu>
+					{adminAppSidebarMenu.map((menu, index) => (
+						<Collapsible key={index} className="group/collapsible">
+							<SidebarMenuItem>
+								<CollapsibleTrigger asChild>
+									<SidebarMenuButton
+										asChild
+										className="font-semibold px-2 py-5"
+									>
+										<Link href={menu?.url}>
+											<menu.icon />
+											{menu.title}
+											{menu.submenu && menu.submenu.length > 0 && (
+												<LuChevronRight className="ml-auto transition-transform duration-200 group-[state=open]/collapsible:rotate-90" />
+											)}
+										</Link>
+									</SidebarMenuButton>
+								</CollapsibleTrigger>
+								{menu.submenu && menu.submenu.length > 0 && (
+									<CollapsibleContent>
+										<SidebarMenuSub>
+											{menu.submenu.map((subMenu, subIndex) => (
+												<SidebarMenuItem key={subIndex}>
+													<SidebarMenuSubButton asChild className="px-2 py-5">
+														<Link href={subMenu.url}>{subMenu.title}</Link>
+													</SidebarMenuSubButton>
+												</SidebarMenuItem>
+											))}
+										</SidebarMenuSub>
+									</CollapsibleContent>
+								)}
+							</SidebarMenuItem>
+						</Collapsible>
+					))}
+				</SidebarMenu>
+			</SidebarContent>
+		</Sidebar>
+	);
+};
+
+export default AppSidebar;
